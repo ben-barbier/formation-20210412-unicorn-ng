7d71ab2ca723416bb855cb9343a45262a5bb058d
Benjamin Barbier <benjamin.barbier.59@gmail.com>
2021-04-13 15:59:28 +0200

    :lipstick: : add card style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { LayoutModule } from '@angular/cdk/layout';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -27,6 +28,7 @@ import { UnicornListComponent } from './unicorn-list/unicorn-list.component';
         MatToolbarModule,
         MatSidenavModule,
         MatListModule,
+        MatCardModule,
     ],
     providers: [],
     bootstrap: [AppComponent],
