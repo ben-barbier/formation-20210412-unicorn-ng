ef889546a8d5775c9086d5e1e8ac8f1fea8003b9
Benjamin Barbier <benjamin.barbier.59@gmail.com>
2021-04-13 14:38:48 +0200

    :heavy_plus_sign: : add @angular/material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UnicornCardComponent } from './unicorn-list/unicorn-card/unicorn-card.component';
@@ -8,7 +9,7 @@ import { UnicornListComponent } from './unicorn-list/unicorn-list.component';
 
 @NgModule({
     declarations: [AppComponent, UnicornListComponent, UnicornCardComponent],
-    imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+    imports: [BrowserModule, AppRoutingModule, HttpClientModule, BrowserAnimationsModule],
     providers: [],
     bootstrap: [AppComponent],
 })
