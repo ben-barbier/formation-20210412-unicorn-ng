75419592b473f771b23848584c60024b7dd56ab4
Benjamin Barbier <benjamin.barbier.59@gmail.com>
2021-04-13 15:44:11 +0200

    :sparkles: : add navigation style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,33 @@
+import { LayoutModule } from '@angular/cdk/layout';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './shared/nav/nav.component';
 import { UnicornCardComponent } from './unicorn-list/unicorn-card/unicorn-card.component';
 import { UnicornListComponent } from './unicorn-list/unicorn-list.component';
 
 @NgModule({
-    declarations: [AppComponent, UnicornListComponent, UnicornCardComponent],
-    imports: [BrowserModule, AppRoutingModule, HttpClientModule, BrowserAnimationsModule],
+    declarations: [AppComponent, UnicornListComponent, UnicornCardComponent, NavComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        BrowserAnimationsModule,
+        MatButtonModule,
+        MatIconModule,
+        LayoutModule,
+        MatToolbarModule,
+        MatSidenavModule,
+        MatListModule,
+    ],
     providers: [],
     bootstrap: [AppComponent],
 })
